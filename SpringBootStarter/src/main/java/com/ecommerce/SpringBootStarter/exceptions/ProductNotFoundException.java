6b676d593382e46f4e1b4ba0746383c88f262753
Suramanjari G G <manjarisekaran781@gmail.com>
2022-08-23 20:06:51 +0530

    changes

---
+++ SpringBootStarter/src/main/java/com/ecommerce/SpringBootStarter/exceptions/ProductNotFoundException.java
@@ -0,0 +1,11 @@
+package com.ecommerce.SpringBootStarter.exceptions;
+
+public class ProductNotFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+
+}

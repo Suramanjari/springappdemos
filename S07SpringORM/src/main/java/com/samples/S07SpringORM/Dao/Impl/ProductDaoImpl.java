24fa7279100ad550dfce4039127c4e463a53daaa
Suramanjari G G <manjarisekaran781@gmail.com>
2022-08-24 23:53:16 +0530

    changes

--- S07SpringORM/src/main/java/com/samples/S07SpringORM/Dao/Impl/ProductDaoImpl.java
+++ S07SpringORM/src/main/java/com/samples/S07SpringORM/Dao/Impl/ProductDaoImpl.java
@@ -1,5 +1,9 @@
 package com.samples.S07SpringORM.Dao.Impl;
 
+import java.io.Serializable;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Component;
@@ -14,8 +18,13 @@ public class ProductDaoImpl implements ProductDAO {
 	private HibernateTemplate hibernateTemplate;
 
 	@Override
+	@Transactional
 	public int create(Product product) {
 		Integer result = (Integer) hibernateTemplate.save(product);
 		return result;
 	}
+	
+	
+
+
 }

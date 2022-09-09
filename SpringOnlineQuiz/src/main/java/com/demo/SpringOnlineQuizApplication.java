e9e176ca25dbdc8432d35b60d78c7d490cbee3c1
Suramanjari G G <manjarisekaran781@gmail.com>
2022-09-09 20:58:47 +0530

    changes

---
+++ SpringOnlineQuiz/src/main/java/com/demo/SpringOnlineQuizApplication.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories; 
+
+@SpringBootApplication(scanBasePackages = "com") 
+@EntityScan("com.bean")
+@EnableJpaRepositories("com.repository") 
+
+public class SpringOnlineQuizApplication {
+public static void main(String[] args) { SpringApplication.run(SpringOnlineQuizApplication.class, args);
+}
+}
+
